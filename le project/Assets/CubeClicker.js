3b01a688a8f4e489831cd90afa872617640957f2
Hitman666 <nikola.breznjak@gmail.com>
2015-01-09 14:40:10 +0100

    finished

---
+++ le project/Assets/CubeClicker.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var counter = 0;
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnMouseDown(){
+	Debug.Log("clicked " + counter++);
+	var fadeTime = GameObject.Find ("FaderObj").GetComponent(FadingJS).BeginFade(1);		
+	
+	yield WaitForSeconds(fadeTime);	
+	
+	GameObject.Find("Cube").renderer.enabled = false;
+	
+	fadeTime = GameObject.Find ("FaderObj").GetComponent(FadingJS).BeginFade(-1);
+	
+	yield WaitForSeconds(fadeTime);	
+	
+	Application.LoadLevel(Application.loadedLevel + 1);	
+}
